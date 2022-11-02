15b7d2f42872cd7b94aa13d6a85857a777499790
Kumar Anirudha <mail@anirudha.dev>
2022-11-02 16:07:27 +0530

    google slides public embed

--- website/components/utils.js
+++ website/components/utils.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { Viewer, SpecialZoomLevel, Worker } from "@react-pdf-viewer/core";
 
-const deck = "https://dripdoc.s3.amazonaws.com/v1.pdf";
-
-export function Presentation() {
+export function Presentation({ url }) {
   return (
-    <div>
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js">
-        <Viewer fileUrl={deck} defaultScale={SpecialZoomLevel.PageFit} />
-      </Worker>
-    </div>
+    <iframe
+      src={url}
+      frameBorder="0"
+      width="1280"
+      height="720"
+      allowFullScreen={true}
+      mozallowfullscreen="true"
+      webkitallowfullscreen="true"
+    ></iframe>
   );
 }
